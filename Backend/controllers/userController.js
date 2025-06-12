fa21bbfce5ca70a8d763289e78a6cca0f8eebafa
Alazar Girmachew <alazargirmachew10@gmail.com>
2025-06-12 18:38:25 +0300

    Version Backend 3

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -94,8 +94,20 @@ const registerUser = async (req, res) => {
 };
 
 const adminLogin = async (req, res) => {
+    try {
+        
+        const {email, password} = req.body
 
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success:true, token})
+        } else{
+            res.json({success:false, message:"Invalid Email or Password"})
+        }
 
+    } catch (error) {
+        
+    }
 
 }
 
--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema({
      description: {type: String, required: true},
      price: { type: Number, required: true},
      image: {type: Array, required: true},
-     Category: { type: String, required: true},
+     category: { type: String, required: true},
      sizes: { type: Array, required: true},
      date: { type: Number, required: true}
 })
